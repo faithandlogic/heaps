782cf9b44035ff5b09a93b6c0ecca7bda3a62a0d
Fatih <fatihsen539@gmail.com>
2022-03-13 23:46:19 -0700

    Update heaps.go

--- heaps.go
+++ heaps.go
@@ -1,8 +1,86 @@
 package main
 
-import(
-)
+import "fmt"
+
+type MaxHeap struct {
+	array []int
+}
+
+func (heap *MaxHeap) Insert(data int) {
+	heap.array = append(heap.array,data)
+	heap.add(len(heap.array)-1)
+}
+
+func (heap *MaxHeap) add(index int) {
+	for heap.array[parent(index)] < heap.array[index] {
+		heap.swap(parent(index), index)
+		index = parent(index)
+	}
+}
+
+func parent(i int) int {
+	return (i-1)/2
+}
+
+func left(i int) int {
+	return 2*i+1
+}
+func right(i int) int {
+	return 2*1+2
+}
+
+func (heap *MaxHeap) swap(i1, i2 int) {
+	heap.array[i1], heap.array[i2] = heap.array[i2], heap.array[i1]
+}
+
+func (heap *MaxHeap) Remove() int {
+	extracted := heap.array[0]
+	l := len(heap.array)-1
+	if l == 0 {
+		fmt.Println("can't extract, len = 0")
+		return -1
+	}
+	heap.array[0] = heap.array[l]
+	heap.array = heap.array[:l]
+	heap.restore(0)
+	return extracted
+}
+
+func (heap *MaxHeap) restore(index int) {
+	lastIndex := len(heap.array)-1
+	l,r := left(index), right(index)
+	compare := 0
+	for l <= lastIndex {
+		if l == lastIndex {
+			compare = l
+		} else if heap.array[l] > heap.array[r] {
+			compare = l
+		} else {
+			compare = r
+		}
+
+		if heap.array[index] < heap.array[compare] {
+			heap.swap(index, compare)
+			index = compare
+			l,r = left(index), right(index)
+		} else {
+			return
+		}
+	} 
+}
 
 func main() {
-	print("Heaps")
+	println("Heaps")
+
+	heap := &MaxHeap{}
+	buildHeap := []int{12,22,32,5,3,7,2,23,44,32,11}
+	for _, v := range buildHeap {
+		heap.Insert(v)
+	}
+	fmt.Println(heap)
+	for i := 0; i < 5; i++ {
+		heap.Remove()
+	}
+	fmt.Println(heap)
+
 }
